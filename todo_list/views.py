3f4133c9136720f8f08d22b921c48c02e159e91a
Navin-66 <113427260+Navin-66@users.noreply.github.com>
2023-11-09 14:05:32 +0530

    Update views.py

--- todo_list/views.py
+++ todo_list/views.py
@@ -4,7 +4,31 @@ from django.http import HttpResponseRedirect
 from .models import List
 from .forms import ListForm
 
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from .models import List
+from .forms import ListForm
 
+from django.http import JsonResponse
+from botbuilder.schema import Activity
+
+from botbuilder.core import BotFrameworkAdapter
+
+def handle_bot_messages(request):
+    if request.method == 'POST':
+        activity_data = request.body.decode('utf-8')
+        activity = Activity().deserialize(activity_data)
+        
+        # Create an instance of BotFrameworkAdapter
+        adapter = BotFrameworkAdapter(app_id='46f57800-deac-4cb5-8d89-e245b135754c', app_password='b2eb69b3-5cce-45a4-8286-1b73c5d9fa41')
+        
+        # Implement your bot logic here
+        # You can send a response back to the user using adapter.send_activities()
+
+        return JsonResponse({'status': 'success'})
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
 def home(request):
     try:
         if request.method == "POST":
