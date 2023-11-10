d99ebac4e19930bf404a4ae3353ab72f05eab37e
Navin-66 <113427260+Navin-66@users.noreply.github.com>
2023-11-10 14:14:18 +0530

    Update settings.py

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-l80arx#auha@m#-&9kuj#b$61yeh2ly!gnr$8oy(43!s2wfq2^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['todolistmagic.azurewebsites.net']
+ALLOWED_HOSTS = ['todoappmagic.azurewebsites.net']
 
 
 # Application definition
